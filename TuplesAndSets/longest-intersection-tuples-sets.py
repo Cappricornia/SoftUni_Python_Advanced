bf1d448c8f4ad3c5ef239970a61228ccafc01a53
Diyana Hristova <90700181+Cappricornia@users.noreply.github.com>
2023-01-27 22:23:45 +0200

    Lists as stacks and queues

--- TuplesAndSets/longest-intersection-tuples-sets.py
+++ TuplesAndSets/longest-intersection-tuples-sets.py
@@ -1,10 +1,10 @@
-# Write a program which finds the longest intersection. You will be given a number N. On each of the next N lines
-# you will be given two ranges in the format: "{first_start},{first_end}-{second_start},{second_end}". You should
-# find the intersection of these two ranges. The start and end numbers in the ranges are inclusive.
-# Finally, you should find the longest intersection of all N intersections, print the numbers that are included and
-# its length in the format: "Longest intersection is [{longest_intersection_numbers}] with length
-# {length_longest_intersection}"
-# Note: in each range, there will always be intersection. If there are two equal intersections, print the first one.
+"""Write a program which finds the longest intersection. You will be given a number N. On each of the next N lines
+you will be given two ranges in the format: "{first_start},{first_end}-{second_start},{second_end}". You should
+find the intersection of these two ranges. The start and end numbers in the ranges are inclusive.
+Finally, you should find the longest intersection of all N intersections, print the numbers that are included and
+its length in the format: "Longest intersection is [{longest_intersection_numbers}] with length
+{length_longest_intersection}"
+Note: in each range, there will always be intersection. If there are two equal intersections, print the first one."""
 
 
 
