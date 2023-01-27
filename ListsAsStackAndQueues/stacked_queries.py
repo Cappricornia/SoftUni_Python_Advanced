e5d6baa1df25e852b2cf7b327f6323a483e0169d
Diyana Hristova <90700181+Cappricornia@users.noreply.github.com>
2023-01-27 22:20:30 +0200

    Lists as stacks and queues

--- ListsAsStackAndQueues/stacked_queries.py
+++ ListsAsStackAndQueues/stacked_queries.py
@@ -1,11 +1,11 @@
-# You have an empty sequence. You will receive an integer – N. On the next N lines you will receive queries.
-# Each query is one of these four types:
-# '1 x' – Push the element x into the stack.
-# '2' – Delete the element at the top of the stack.
-# '3' – Print the maximum element in the stack.
-# '4' – Print the minimum element in the stack.
-# After you go through all the queries, print the stack from the top to the bottom in the following format:
-# "{n}, {n1}, {n2} …, {nn}"
+"""You have an empty sequence. You will receive an integer – N. On the next N lines you will receive queries.
+Each query is one of these four types:
+'1 x' – Push the element x into the stack.
+'2' – Delete the element at the top of the stack.
+'3' – Print the maximum element in the stack.
+'4' – Print the minimum element in the stack.
+After you go through all the queries, print the stack from the top to the bottom in the following format:
+'{n}, {n1}, {n2} …, {nn}' """
 
 n = int(input())
 stack = []
