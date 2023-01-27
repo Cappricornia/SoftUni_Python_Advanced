7c4ca17b1580ea73f6cf283b723c9855e0bd26ed
Diyana Hristova <90700181+Cappricornia@users.noreply.github.com>
2023-01-27 22:24:14 +0200

    Lists as stacks and queues

--- TuplesAndSets/periodic-table-tuples_sets.py
+++ TuplesAndSets/periodic-table-tuples_sets.py
@@ -1,7 +1,7 @@
-# Write a program that keeps all the unique chemical elements. On the first line you will be given a number
-# n - the count of input lines that you are going to receive. On the next n lines, you will be receiving chemical
-# compounds, separated by a single space. Your task is to print all the unique ones on separate lines
-# (the order does not matter):
+"""Write a program that keeps all the unique chemical elements. On the first line you will be given a number
+n - the count of input lines that you are going to receive. On the next n lines, you will be receiving chemical
+compounds, separated by a single space. Your task is to print all the unique ones on separate lines
+(the order does not matter):"""
 
 
 
