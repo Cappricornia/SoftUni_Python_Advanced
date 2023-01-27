df69623fbd19e6290f36046f9e8b26f76c8d8c33
Diyana Hristova <90700181+Cappricornia@users.noreply.github.com>
2023-01-27 22:24:41 +0200

    Lists as stacks and queues

--- TuplesAndSets/sets-of-elements-set.py
+++ TuplesAndSets/sets-of-elements-set.py
@@ -1,11 +1,11 @@
-# Write a program which prints a set of elements. On the first line, you will receive two numbers - n and m,
-# separated by a single space - they represent the lengths of two separate sets. On the next n + m lines you
-# will receive n numbers, which are the numbers in the first set, and m numbers, which are in the second set.
-# Find all the unique elements that appear in both and print them on separate lines (the order does not matter).
-# For example:
-# Set with length n = 4: {1, 3, 5, 7}
-# Set with length m = 3: {3, 4, 5}
-# Set that contains all the elements that repeat in both sets -> {3, 5}
+"""Write a program which prints a set of elements. On the first line, you will receive two numbers - n and m,
+separated by a single space - they represent the lengths of two separate sets. On the next n + m lines you
+will receive n numbers, which are the numbers in the first set, and m numbers, which are in the second set.
+Find all the unique elements that appear in both and print them on separate lines (the order does not matter).
+For example:
+Set with length n = 4: {1, 3, 5, 7}
+Set with length m = 3: {3, 4, 5}
+Set that contains all the elements that repeat in both sets -> {3, 5}"""
 
 
 
