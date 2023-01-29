0108d432b639e7515e11c27bd0bf7c15230ccf67
Diyana Hristova <90700181+Cappricornia@users.noreply.github.com>
2023-01-29 14:26:19 +0200

    Functions advanced

---
+++ FunctionsAdvanced/keyword_args_length.py
@@ -0,0 +1,11 @@
+'''Create a function called kwargs_length() that can receive some keyword arguments and return their length.
+Submit only the function in the judge system.'''
+
+
+def kwargs_length(**kwargs):
+    return len(kwargs)
+
+
+print(kwargs_length(name='Peter', аге=25))
+
+
